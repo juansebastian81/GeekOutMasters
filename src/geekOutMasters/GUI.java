c383290160bc28c97bfb32bd19b355981eea1320
Juan Sebastian Ospina Maya 2041554 <juan.sebastian.ospina@correounivalle.edu.co>
2022-01-14 23:07:02 -0500

    create class GUI

--- src/geekOutMasters/GUI.java
+++ src/geekOutMasters/GUI.java
@@ -11,7 +11,23 @@ import java.awt.*;
  */
 public class GUI extends JFrame {
 
+    private static final String MENSAJE_INICIO="Bienvenido a Craps \n"
+            +"Oprime el boton lanzar para iniciar el juego"
+            +"\nSi tu tiro de salida es 7 u 11 ganas con Natural"
+            +"\nSi tu tiro de salida es 2, 3 u 11 pierdes con Craps"
+            +"\nSi sacas cualquier otro valor estableceras el Punto"
+            +"\nEstado en punto podras seguir lanzando los dados"
+            +"\npero ahora ganaras si vueles a sacar el valor de punto"
+            +"\nsin que previamente hayas sacado 7";
+
     private Header headerProject;
+    private JLabel dado1, dado2, dado3, dado4, dado5, dado6, dado7, dado8, dado9, dado10;
+    private JButton ayuda,salir,lanzar, limpiar;
+    private JPanel panelDadosInactivos, panelDadosUtilizados, panelDadosActivos;
+    private ImageIcon imageDado;
+    private JTextArea mensajeSalida, resultadosDados;
+    //private Escucha escucha;
+    private ModelGOM modelGOM;
 
     /**
      * Constructor of GUI class
@@ -20,9 +36,8 @@ public class GUI extends JFrame {
         initGUI();
 
         //Default JFrame configuration
-        this.setTitle("The Title app");
-        this.setSize(200,100);
-        //this.pack();
+        this.setUndecorated(true);
+        this.pack();
         this.setResizable(true);
         this.setVisible(true);
         this.setLocationRelativeTo(null);
@@ -34,12 +49,127 @@ public class GUI extends JFrame {
      * create Listener and control Objects used for the GUI class
      */
     private void initGUI() {
-        //Set up JFrame Container's Layout
-        //Create Listener Object and Control Object
-        //Set up JComponents
-        headerProject = new Header("Header ...", Color.BLACK);
 
-        this.add(headerProject,BorderLayout.NORTH); //Change this line if you change JFrame Container's Layout
+        //Set up JFrame Container's Layout
+        this.getContentPane().setLayout(new GridBagLayout());
+        GridBagConstraints constraints = new GridBagConstraints();
+
+        //Create Listener Object and Control Object
+        //escucha = new Escucha();
+        modelGOM = new ModelGOM();
+
+        //Set up JComponents
+        headerProject = new Header("Mesa de Juego Geek out Masters", Color.BLACK);
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.gridwidth = 2;
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        this.add(headerProject,constraints);
+
+        ayuda = new JButton(" ? ");
+        //ayuda.addActionListener();
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        constraints.gridwidth = 1;
+        constraints.fill = GridBagConstraints.NONE;
+        constraints.anchor = GridBagConstraints.LINE_START;
+        this.add(ayuda,constraints);
+
+        salir = new JButton(" Salir ");
+        //salir.addActionListener();
+        constraints.gridx = 1;
+        constraints.gridy = 1;
+        constraints.gridwidth = 1;
+        constraints.fill = GridBagConstraints.NONE;
+        constraints.anchor = GridBagConstraints.LINE_END;
+        this.add(salir,constraints);
+
+        imageDado = new ImageIcon(getClass().getResource("/resources/dado.png"));
+        dado1 = new JLabel(imageDado);
+        dado2 = new JLabel(imageDado);
+        dado3 = new JLabel(imageDado);
+        dado4 = new JLabel(imageDado);
+        dado5 = new JLabel(imageDado);
+        dado6 = new JLabel(imageDado);
+        dado7 = new JLabel(imageDado);
+        dado8 = new JLabel(imageDado);
+        dado9 = new JLabel(imageDado);
+        dado10 = new JLabel(imageDado);
+
+        //panel 1
+        panelDadosInactivos = new JPanel();
+        panelDadosInactivos.setPreferredSize(new Dimension(400,220));
+        panelDadosInactivos.setBorder(BorderFactory.createTitledBorder(" Dados Inactivos "));
+        panelDadosInactivos.add(dado1);
+        panelDadosInactivos.add(dado2);
+        panelDadosInactivos.add(dado3);
+        constraints.gridx = 0;
+        constraints.gridy = 2;
+        constraints.gridwidth = 1;
+        constraints.fill = GridBagConstraints.BOTH;
+        constraints.anchor = GridBagConstraints.CENTER;
+        this.add(panelDadosInactivos,constraints);
+
+        //panel 2
+        panelDadosUtilizados = new JPanel();
+        panelDadosUtilizados.setPreferredSize(new Dimension(400,220));
+        panelDadosUtilizados.setBorder(BorderFactory.createTitledBorder(" Dados Utilizados "));
+        constraints.gridx = 1;
+        constraints.gridy = 2;
+        constraints.gridwidth = 1;
+        constraints.fill = GridBagConstraints.BOTH;
+        constraints.anchor = GridBagConstraints.CENTER;
+        this.add(panelDadosUtilizados,constraints);
+
+        //panel 3
+        panelDadosActivos = new JPanel();
+        panelDadosActivos.setPreferredSize(new Dimension(400,150));
+        panelDadosActivos.setBorder(BorderFactory.createTitledBorder(" Dados Activos "));
+        panelDadosActivos.add(dado4);
+        panelDadosActivos.add(dado5);
+        panelDadosActivos.add(dado6);
+        panelDadosActivos.add(dado7);
+        panelDadosActivos.add(dado8);
+        panelDadosActivos.add(dado9);
+        panelDadosActivos.add(dado10);
+        constraints.gridx = 0;
+        constraints.gridy = 3;
+        constraints.gridwidth = 2;
+        constraints.fill = GridBagConstraints.BOTH;
+        constraints.anchor = GridBagConstraints.CENTER;
+        this.add(panelDadosActivos,constraints);
+
+
+        mensajeSalida = new JTextArea(4,29);
+        mensajeSalida.setText(" Usa el boton (?) para ver las reglas del juego ");
+        mensajeSalida.setBorder(BorderFactory.createTitledBorder(" Tablero "));
+        mensajeSalida.setEditable(false);
+        constraints.gridx = 0;
+        constraints.gridy = 5;
+        constraints.gridwidth = 1;
+        constraints.gridheight = 2;
+        constraints.fill = GridBagConstraints.BOTH;
+        constraints.anchor = GridBagConstraints.CENTER;
+        this.add(mensajeSalida,constraints);
+
+        lanzar = new JButton("lanzar");
+        //lanzar.addActionListener();
+        constraints.gridx = 1;
+        constraints.gridy = 4;
+        constraints.gridwidth = 2;
+        constraints.fill = GridBagConstraints.NONE;
+        constraints.anchor = GridBagConstraints.CENTER;
+        this.add(lanzar,constraints);
+
+        limpiar = new JButton("limpiar");
+        //limpiar.addActionListener();
+        constraints.gridx = 1;
+        constraints.gridy = 5;
+        constraints.gridwidth = 2;
+        constraints.fill = GridBagConstraints.NONE;
+        constraints.anchor = GridBagConstraints.CENTER;
+        this.add(limpiar,constraints);
+
     }
 
     /**
@@ -53,10 +183,6 @@ public class GUI extends JFrame {
         });
     }
 
-    /**
-     * inner class that extends an Adapter Class or implements Listeners used by GUI class
-     */
-    private class Escucha {
 
-    }
 }
+
--- src/geekOutMasters/ModelGOM.java
+++ src/geekOutMasters/ModelGOM.java
@@ -1,4 +1,137 @@
 package geekOutMasters;
 
 public class ModelGOM {
+
+    /**
+     * ModelCraps apply craps rules.
+     * estado = 1 natural winner
+     * estado = 2 craps looser
+     * estado = 3 establish punto
+     * estado = 4 punto winner
+     * estado = 5 punto looser
+     * @author Juan Senastian Ospina Maya
+     * @version v1.0.0 date 08/12/2021
+     */
+    public class ModelCraps {
+
+        private Dado dado1, dado2;
+        private int[] caras;
+        private int tiro, punto, estado, flag;
+        private String[] estadoToString;
+
+        public ModelCraps(){
+
+            dado1 = new Dado();
+            dado2 = new Dado();
+            caras = new int[2];
+            estadoToString = new String[2];
+            flag = 0;
+
+        }
+
+        /**
+         * Establish the tiro value according to each dice
+         */
+        public void calcularTiro(){
+
+            caras[0] = dado1.getCara();
+            caras[1] =  dado2.getCara();
+            tiro = caras[0] + caras[1];
+
+        }
+
+        /**
+         * Establish game state according to estado attribute value
+         * estado = 1 natural winner
+         * estado = 2 craps looser
+         * estado = 3 establish punto
+         */
+        public void determinarJuego(){
+
+            if(flag == 0){
+                if(tiro == 7 || tiro == 11){
+                    estado = 1;
+                }else {
+                    if(tiro == 2 || tiro == 3 || tiro == 12){
+                        estado = 2;
+                    }else {
+                        estado = 3;
+                        punto = tiro;
+                        flag = 1;
+                    }
+                }
+            }else {
+
+                rondaPunto();
+            }
+        }
+
+        /**
+         * Establish game state according to estado attribute value
+         * estado = 4 punto winner
+         * estado = 5 punto looser
+         */
+        private void rondaPunto() {
+
+            if(tiro == punto){
+                estado = 4;
+                flag = 0;
+            }else {
+                if(tiro == 7){
+                    estado = 5;
+                    flag = 0;
+
+                }else {
+                    estado = 6;
+                }
+            }
+
+        }
+
+        public int[] getCaras() {
+            return caras;
+        }
+
+        public int getTiro() {
+            return tiro;
+        }
+
+        public int getPunto() {
+            return punto;
+        }
+
+        /**
+         * Establish message game state according to estado attribute value
+         * @return Message for view class
+         */
+        public String[] getEstadoToString() {
+            switch(estado){
+                case 1: estadoToString[0] = "Tiro de salida = " + tiro;
+                    estadoToString[1] = "Sacaste Natural, has ganado!!!";
+                    break;
+                case 2: estadoToString[0] = "Tiro de salida = " + tiro;
+                    estadoToString[1] = "Sacaste Craps, has perdido!!!";
+                    break;
+                case 3: estadoToString[0] = "Tiro de salida = " + tiro + "\nPunto = " + punto;
+                    estadoToString[1] = "Estableciste punto en " + punto + ", debes seguir lanzando!!!" +
+                            "\nPero si sacas 7 antes que "+ punto +" perderas";
+                    break;
+                case 4: estadoToString[0] = "Tiro de salida = " + punto + "\nPunto = " + punto
+                        +"\nEl valor del nuevo tiro = " + tiro;
+                    estadoToString[1] = "Volviste a sacar " + punto + " has ganado!!!";
+                    break;
+                case 5: estadoToString[0] = "Tiro de salida = " + punto + "\nPunto = " + punto
+                        +"\nEl valor del nuevo tiro = " + tiro;
+                    estadoToString[1] = "Sacaste 7 antes que " + punto + ", has perdido!!!";
+                    break;
+                case 6: estadoToString[0] = "Tiro de salida = " + punto + "\nPunto = " + punto
+                        +"\nEl valor del nuevo tiro = " + tiro;
+                    estadoToString[1] = "Estableciste punto en " + punto + ", debes seguir lanzando!!!" +
+                            "\nPero si sacas 7 antes que "+ punto +" perderas";
+                    break;
+
+            }
+            return estadoToString;
+        }
+    }
 }

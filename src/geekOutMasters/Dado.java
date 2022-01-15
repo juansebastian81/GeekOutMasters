d656b9bc2ada2a3480dd260c4d75db2a39d6cb5f
Juan Sebastian Ospina Maya 2041554 <juan.sebastian.ospina@correounivalle.edu.co>
2022-01-14 20:34:09 -0500

    create class dice

--- src/geekOutMasters/Dado.java
+++ src/geekOutMasters/Dado.java
@@ -1,4 +1,24 @@
 package geekOutMasters;
 
+import java.util.Random;
+
+/**
+ * Class Dado generate a Random value between 1 and 6
+ * @author Leidy Estefania Parra Concha
+ * Juan Sebastian Ospina Maya
+ * @version v1.0.0 date 14/01/2021
+ */
 public class Dado {
+
+    private int cara;
+
+    /**
+     * Method that generate and random value to cara
+     * @return number between (1,6)
+     */
+    public int getCara() {
+        Random aleatorio = new Random();
+        cara = aleatorio.nextInt(6) + 1;
+        return cara;
+    }
 }
